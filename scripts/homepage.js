a92906063d02571d18739a84803de83e2828083e
CarlosMedinaSullca <carlos.medina.sullca@gmail.com>
2024-12-02 06:29:28 -0400

    W3 learning activities completed

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -130,3 +130,35 @@ function displayCourses(filteredCourses) {
     document.querySelector(".credits").appendChild(creditsTodisplay)
 
 }
+// -21.517605167989498, -64.72949905980894
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector(".fig");
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-21.52&lon=-64.73&units=metric&appid=48a9c5117dce7d7efbd2633a23faf9d3';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        } 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+  
+
+function displayResults (data) {
+    currentTemp.innerHTML = `${data.main.temp} &deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt',`${data.weather[0].description}`);
+    captionDesc.textContent = `${desc}`;
+}
